74891e710a691bd86c7194dfbb79e26ee2f64e3d
hubligpt5-hub <hubligpt5@gmail.com>
2026-03-20 14:53:51 +0530

    Add files via upload

---
+++ anmol.java
@@ -0,0 +1,13 @@
+class anmol 
+{
+ public static void main(String a[]) 
+ {
+ System.out.println("enter a name");
+ Scanner sc = new scanner(System.in);
+ String name = sc.next();
+ System.out.println("name of Student" +name);
+ System.out.println(" enter reg no");
+ int reg_no = sc.next();
+ System.out.println("reg no is "+ reg_no);
+ }
+}
